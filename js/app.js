a795cdb6fbd855e0ff48d9cfcb6c90e60da70667
Rafael <rafiepatel@gmail.com>
2016-02-10 17:47:38 -0500

    Add mobile detection.

--- js/app.js
+++ js/app.js
@@ -10,11 +10,32 @@ var requestAnimFrame = (function(){
         };
 })();
 
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-if (canvas.width >= 500) {
+if (!isMobile.any()) {
 		canvas.width  = 320;
 		canvas.height = 480;
 		canvas.style.border = "1px solid #000";
